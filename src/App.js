977d04db1961e8e4eacee6d1aca373bdfa8e6ede
Dibyajyoti Sahoo <dibyajyotisahoo2504@gmail.com>
2023-05-21 23:09:23 +0530

    Routing Files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./Components/Home";
+import Navbar from "./Components/Navbar";
+import Create from "./Components/Create";
+import BlogDetails from "./Components/BlogDetails";
+import NotFound from "./Components/NotFound";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route exact path="/create" element={<Create />} />
+            <Route exact path="/blogs/:id" element={<BlogDetails />} />
+            <Route exact path="/blogs/:id" element={<BlogDetails />} />
+            <Route exact path="*" element={<NotFound />} />
+          </Routes>
+        </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
